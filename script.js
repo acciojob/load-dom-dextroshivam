02740b492b0780dd25d19391e709eaa0d6719412
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-02-04 16:34:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	console.log("DOM load success");
+});
+// document.addEventListener('DOMContentLoaded', function() {
+//   console.log('The DOMContentLoaded event has fired!');
+// });
