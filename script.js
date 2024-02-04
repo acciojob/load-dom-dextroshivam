9e527910cdee4adbfb59a13e55b292bbe3d2739e
shivam <76024545+dextroshivam@users.noreply.github.com>
2024-02-04 16:44:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",()=>{
-	console.log("DOM load success");
+	// console.log("DOM load success");
+	document.body.textContent = "DOM load success";
 });
 // document.addEventListener('DOMContentLoaded', function() {
 //   console.log('The DOMContentLoaded event has fired!');
